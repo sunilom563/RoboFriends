716cdd54fe5ed5380d95f0377b44c557244ceebd
sunilom563 <sunilbdeshanur@gmail.com>
2022-05-07 21:55:58 +0200

    minor changes

--- src/CardList.js
+++ src/CardList.js
@@ -7,13 +7,17 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
-    const cardComponent = robots.map((user,i) => {
-        return <Card  id={robots[i].id} name={robots[i].username} email={robots[i].email}/>
-    })
+    
 
     return(
        <div>
-       {cardComponent}
+       {robots.map((user,i) => {
+        return (<Card  
+                key={i}
+                id={robots[i].id} 
+                name={robots[i].username} 
+                email={robots[i].email}/>)
+         })}
        </div>
     );
 
